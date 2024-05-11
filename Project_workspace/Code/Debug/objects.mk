da4005c9b917f23cea4d8bd8e4bcd07ca2b5af68
Ahmed Mostafa <108436896+Sasa-Indeed@users.noreply.github.com>
2024-05-11 15:25:12 +0300

    Delete Project_workspace directory

--- Project_workspace/Code/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"D:/ASU/4-Senior1/Fall\ 23/CSE211\ Introduction\ to\ Embedded\ Systems/Tivaware/driverlib/ccs/Debug/driverlib.lib"
-
